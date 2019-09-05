cd10bf7d64cf815902a8b62f6222d5eb601c528e
Yurii Tokarskyi <ytokarskyi@lemonunit.com>
2019-09-05 06:37:44 +0200

    Updates

---
+++ precache-manifest.c584f5d913c0b0201d2156ce89656309.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1627c8772e3e6de5cfc4b8159488d5",
+    "url": "/csv-editor-online/index.html"
+  },
+  {
+    "revision": "441c4d8b327882e3ed2b",
+    "url": "/csv-editor-online/static/css/2.12b24f9e.chunk.css"
+  },
+  {
+    "revision": "c147fbc2812fa791f98c",
+    "url": "/csv-editor-online/static/css/main.b63a8869.chunk.css"
+  },
+  {
+    "revision": "441c4d8b327882e3ed2b",
+    "url": "/csv-editor-online/static/js/2.83c4e7e2.chunk.js"
+  },
+  {
+    "revision": "c147fbc2812fa791f98c",
+    "url": "/csv-editor-online/static/js/main.88fc8f8e.chunk.js"
+  },
+  {
+    "revision": "8d7f285278745e93cdeb",
+    "url": "/csv-editor-online/static/js/runtime~main.c91a9861.js"
+  }
+]);
